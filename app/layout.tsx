445f6253023becd227b07fbf9682daf102020eff
Rob Beaumont <robbieabeaumont@gmail.com>
2026-05-31 13:54:31 -0400

    fix: add suppressHydrationWarning to html element to persist theme across refreshes

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,7 @@ const jsonLd = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
